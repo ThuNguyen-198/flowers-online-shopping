04ee0aa5b78254f09454b8e16bf5bd6151456428
Uyen Doan <catuyen99@gmail.com>
2023-03-03 23:25:15 -0600

    Merged main

--- flowers-online-shopping/src/app/app-routing.module.ts
+++ flowers-online-shopping/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
 
 const routes: Routes = [
   {path: '', component: RegisterPageComponent},
+  {path: 'checkout', component: CheckoutPageComponent}
 //   {path: 'signup', component: SignUpComponent},
 //   {path: 'flower-list', component: FlowerListComponent},
 //   {path: 'flower-create', component: FlowerCreateComponent}
