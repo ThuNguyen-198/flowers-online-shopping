fd5fab4fe8bbf07b729ed73fe30552b4a4c4eab0
Uyen Doan <catuyen99@gmail.com>
2023-03-17 13:00:56 -0500

    added API

--- flowers-online-shopping/backend/middleware/app.js
+++ flowers-online-shopping/backend/middleware/app.js
@@ -3,25 +3,27 @@
 This JS file is run on server as a "middleman" between backend and frontend.
 Mongoose is utilized in this file to interact with MongoDB.
 */
-
-const express = require('express');
+const dotenv = require("dotenv");
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+const axios = require("axios");
 
-const Bouquet = require('../../backend/models/Bouquet');
-const Color = require('../../backend/models/Color');
-const Flower = require('../../backend/models/Flower');
-const FlowerType = require('../../backend/models/FlowerType');
-const Image = require('../../backend/models/Image');
-const Order = require('../../backend/models/Order');
-const Other = require('../../backend/models/Other');
-const Wrapping = require('../../backend/models/Wrapping');
+const Bouquet = require("../../backend/models/Bouquet");
+const Color = require("../../backend/models/Color");
+const Flower = require("../../backend/models/Flower");
+const FlowerType = require("../../backend/models/FlowerType");
+const Image = require("../../backend/models/Image");
+const Order = require("../../backend/models/Order");
+const Other = require("../../backend/models/Other");
+const Wrapping = require("../../backend/models/Wrapping");
 
+dotenv.config();
 //Initialize routes
-const userRoute = require('../../backend/routes/users')
+const userRoute = require("../../backend/routes/users");
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
 //Connect with MongoDB using username and password of users added on MongoDB Console.
 mongoose
@@ -37,45 +39,46 @@ mongoose
 
 //Allow our app from localhost:4200 to access server in localhost:3000
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 //BodyParser is an NPM package where it process data sent in an HTTP request body.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-  
+
 //POST request: update/add information on MongoDB.
 app.post("/api/flowers", (request, response, next) => {
   const flower = new Flower({
     name: request.body.name,
     color: request.body.color,
     kind: request.body.kind,
-    occasion: request.body.occasion
+    occasion: request.body.occasion,
   });
-  flower.save().then(createdFlower => {
+  flower.save().then((createdFlower) => {
     response.status(201).json({
       message: "Flower added successfully",
-      flowerId: createdFlower._id
+      flowerId: createdFlower._id,
     });
   });
 });
 
-app.get("/api/flowers", (request, response, next) => {
-  Flower.find().then(documents => {
-    response.status(200).json({
-      message: "Flower list fetched successfully!",
-      flowers: documents
-    });
+app.get("/api/flowers", async (request, response, next) => {
+  response = await axios.get(process.env.GETPRODUCT_URL, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + process.env.API_BASE64,
+    },
   });
+  console.log(response.data.PRODUCTS);
 });
 
 app.use("/api/user", userRoute);
