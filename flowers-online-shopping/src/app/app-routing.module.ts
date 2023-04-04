7ec757df23659f3d8ac09ad9de6935f994e52b87
Thu Nguyen <66782390+ThuNguyen-198@users.noreply.github.com>
2023-04-04 00:25:17 -0500

    add custom page

--- flowers-online-shopping/src/app/app-routing.module.ts
+++ flowers-online-shopping/src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { ProductsDisplayPageComponent } from './products-display-page/products-d
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProductBirthdayComponent } from './products-display-page/product-birthday/product-birthday.component';
 import { AuthGuard } from './auth.guard';
+import { ProductCustomComponent } from './products-display-page/product-custom/product-custom.component';
+import { ProductIndividualsComponent } from './products-display-page/product-individuals/product-individuals.component';
 
 const routes: Routes = [
   { path: 'signup', component: RegisterPageComponent },
@@ -20,6 +22,8 @@ const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'all', component: ProductsDisplayPageComponent },
   { path: 'bd', component: ProductBirthdayComponent },
+  { path: 'custom', component: ProductCustomComponent },
+  { path: 'individuals', component: ProductIndividualsComponent },
 ];
 
 @NgModule({
---
+++ flowers-online-shopping/src/app/products-display-page/product-individuals/product-individuals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-individuals',
+  templateUrl: './product-individuals.component.html',
+  styleUrls: ['./product-individuals.component.css']
+})
+export class ProductIndividualsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
