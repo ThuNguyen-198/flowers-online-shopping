71ba0b5d20f5d9ea4681944b7129437530abbf7c
Jacob Knapp <JacobJamesKnapp@gmail.com>
2023-04-11 21:33:11 -0500

    Switched from bcrypt to argon2id with a nice, long hash and salt. You will likely need to re-run `npm install`, maybe even run `npm install argon2`. I did go through the extra effort of making it auto-update the hashes. (Upon successful login with an account whose password was stored with bcrypt, it will use the known-correct password to update pwd to use argon2id.)

--- flowers-online-shopping/backend/routes/users.js
+++ flowers-online-shopping/backend/routes/users.js
@@ -1,8 +1,21 @@
 //For encrypting password
 // HEY!!! We agreed Argon2id! If that's not available for JS, so be it, but there needs to be discussion.
 const bcrypt = require("bcrypt");
+
+const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
 const checkAuth = require("../../src/app/check-auth");
+
+const Options = argon2.Options;
+const h_opts = {
+  hashLength: 256,
+  saltLength: 128,
+  parallelism: 8,
+  // if hashing is too slow, reduce this by 1 order of magnitude
+  memoryCost: 1 << 20,
+  type: argon2.argon2id,
+};
+const testVar = argon2.hash("HelloThere!", h_opts);
 
 const express = require("express");
 const router = express.Router();
@@ -10,16 +23,26 @@ const router = express.Router();
 const Customer = require("../../backend/models/Customer");
 const Employee = require("../../backend/models/Employee");
 
+const { deserialize, serialize } = require("@phc/format");
+
 router.post("/signup/employee", (req, res, next) => {
   const employee = new Employee();
-  bcrypt.hash(req.body.pwd, 10).then((hash) => {
+  /*bcrypt.hash(req.body.pwd, 10).then((hash) => {
     employee = {
       //eID: req.body.eID,
       login_ID: req.body.login_ID,
       pwd: req.body.pwd,
       perms: req.body.perms,
     };
-  });
+  });*/
+  argon2.hash(req.body.pwd, h_opts).then((hash) => {
+    employee = {
+      login_ID: req.body.login_ID,
+      //pwd: req.body.pwd,
+      pwd: hash,
+      perms: req.body.perms,
+    }
+  })
 
   //Save in the DB
   employee
@@ -38,7 +61,7 @@ router.post("/signup/employee", (req, res, next) => {
 });
 
 router.post("/signup/customer", (req, res, next) => {
-  bcrypt.hash(req.body.pwd, 10).then((hash) => {
+  /*bcrypt.hash(req.body.pwd, 10).then((hash) => {
     const customer = new Customer({
       user: req.body.user,
       email: req.body.email,
@@ -64,11 +87,42 @@ router.post("/signup/customer", (req, res, next) => {
           message: err,
         });
       });
+  });*/
+
+  argon2.hash(req.body.pwd, h_opts, raw=false).then((hash) => {
+    //console.log(typeof hash);
+    const customer = new Customer({
+      user: req.body.user,
+      email: req.body.email,
+      pwd: hash,
+      phone: req.body.phone,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    customer
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "Customer account created!",
+          customer: {
+            ...result,
+          },
+        });
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          message: err
+        });
+      });
   });
 });
 
+/*
+Something needs to process the email so it is in standard form (lowercase, if nothing else)
+*/
 router.post("/login", (req, res, next) => {
-  let fetchedUser;
+  let fetchedUser, need_rehash;
   //console.log(Customer.find({ email: req.body.email }));
   Customer.findOne({ email: req.body.email })
     .then((user) => {
@@ -76,15 +130,50 @@ router.post("/login", (req, res, next) => {
         return res.status(401).json({ message: "Auth failed!" });
       }
       fetchedUser = user;
-      return bcrypt.compare(req.body.pwd, user.pwd);
+      const pwd_id = deserialize(fetchedUser.pwd).id;
+      //console.log("user:"); console.log(user);
+      if (pwd_id === "2b") {
+        need_rehash = true;
+        return bcrypt.compare(req.body.pwd, fetchedUser.pwd);
+      }
+      else return argon2.verify(fetchedUser.pwd, req.body.pwd);
     })
     .then((result) => {
-      if (!result) {
+
+      if (fetchedUser === undefined) {
+        //console.log("fetchedUser undefined, presuming this to mean we already sent the status");
+        return;
+      }
+      else if (!result) {
+       // console.log("Sending 401"); console.log(fetchedUser);
         return res.status(401).json({
           message: "Auth failed!",
         });
       }
 
+      need_rehash = need_rehash || argon2.needsRehash(fetchedUser.pwd, h_opts);
+      //console.log("Needs rehash: " + argon2.needsRehash(fetchedUser.pwd, h_opts))
+      //console.log(fetchedUser);
+
+      // if password is correct but options have changed, rehash it.
+      // Plus, this will convert from bcrypt!
+      if (need_rehash) {
+        argon2.hash(req.body.pwd, h_opts).then((hash) => {
+          fetchedUser.pwd = hash;
+          //console.log(fetchedUser);
+          //fetchedUser.updateOne()
+          fetchedUser.save()
+          .then((obj) => {
+            //console.log("Saved!");
+            //console.log(obj);
+          }).catch((err) => {
+            //console.log("Update failed?");
+            console.log(err);
+          });
+        });
+      }
+      
+      // is this REALLY the correct usage?
       const token = jwt.sign(
         {
           email: fetchedUser.email,
@@ -93,12 +182,16 @@ router.post("/login", (req, res, next) => {
         "secret_token_needed_to_be_authorized",
         { expiresIn: "1h" }
       );
-      res.status(200).json({
+      //console.log("Sending 200");
+      return res.status(200).json({
         token: token,
         expiresIn: 3600,
       });
     })
     .catch((err) => {
+      //console.log("Sending 401, err");
+      console.log(err);
+      // should we let them know it was an internal server error? And shouldn't we log the error?
       return res.status(401).json({
         message: "Auth failed!",
       });
